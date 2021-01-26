0bfdd1b04a07774ee7aa1b6de9288407491cd6ba
DhananjayaWijerathne <dmdwijerathne@gmail.com>
2021-01-26 19:58:45 +0800

    Initial commit with pedometer test case

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Morpher_Scripts
+# Single script to run all three tools (Morpher_DFG_Generator, Morpher_CGRA_Mapper, hycube_simulator) to verify the functionality.
+ 
+##########################################################
+# Directory Structure:
+# Morpher Home:
+#     -Morpher_DFG_Generator
+#     -Morpher_CGRA_Mapper
+#     -hycube_simulator
+#     -Morpher_Scripts
+
+###########################################################
+# How to run?
+
+# Build all three tools before running the scripts
+
+# Use python3 virtual environment to run the script
+# https://linoxide.com/linux-how-to/setup-python-virtual-environment-ubuntu/
+
+# python3 -m venv morpher_env
+
+# Set MORPHER_HOME directory as an environment variable (Ex: export MORPHER_HOME=/home/dmd/Workplace/Morphor/github_ecolab_repos)
+
+# python run_morpher.py 
+# Currently, this script contains the pedometer application kernel. 
+# Final Output should be: 
+	Matches ::557, Mismatches::20 
+# Make sure to run this before making any major change to the repos
+
---
+++ run_morpher.py
@@ -0,0 +1,74 @@
+  #!/usr/bin/env python
+import sys
+import os
+import os.path
+import shutil
+############################################
+# Directory Structure:
+# Morpher Home:
+#     -Morpher_DFG_Generator
+#     -Morpher_CGRA_Mapper
+#     -hycube_simulator
+#     -Morpher_Scripts
+
+# Build all three tools before running this script
+
+def main():
+
+  if not 'MORPHER_HOME' in os.environ:
+    raise Exception('Set MORPHER_HOME directory as an environment variable (Ex: export MORPHER_HOME=/home/dmd/Workplace/Morphor/github_ecolab_repos)')
+
+  MORPHER_HOME = os.getenv('MORPHER_HOME')
+  DFG_GEN_HOME = MORPHER_HOME + '/Morpher_DFG_Generator'
+  MAPPER_HOME = MORPHER_HOME + '/Morpher_CGRA_Mapper'
+  SIMULATOR_HOME = MORPHER_HOME + '/hycube_simulator'
+
+  DFG_GEN_KERNEL = DFG_GEN_HOME + '/applications/pedometer/'
+  MAPPER_KERNEL = MAPPER_HOME + '/applications/pedometer/'
+  SIMULATOR_KERNEL =SIMULATOR_HOME + '/applications/pedometer/'
+
+  my_mkdir(DFG_GEN_KERNEL)
+  my_mkdir(MAPPER_KERNEL)
+  my_mkdir(SIMULATOR_KERNEL)
+
+
+
+##############################################################################################################################################
+  print('\nRunning Morpher_DFG_Generator\n')
+  os.chdir(DFG_GEN_KERNEL)
+
+  print('\nGenerating DFG\n')
+  os.system('./run_pass.sh pedometer')
+  os.system('dot -Tpdf pedometer_INNERMOST_LN1_PartPredDFG.dot -o pedometer_INNERMOST_LN1_PartPredDFG.pdf')
+
+  MEM_TRACE = DFG_GEN_KERNEL + '/memtraces'
+
+  my_mkdir(MEM_TRACE)
+
+  print('\nGenerating Data Memory Content\n')
+  os.system('./final')
+  os.system('cp memtraces/loop_pedometer_INNERMOST_LN1_0.txt '+SIMULATOR_KERNEL)
+  os.system('cp pedometer_INNERMOST_LN1_mem_alloc.txt '+SIMULATOR_KERNEL)
+
+##############################################################################################################################################
+  print('\nRunning Morpher_CGRA_Mapper\n')
+  os.chdir(MAPPER_KERNEL)
+
+  os.system('cp pedometer_INNERMOST_LN1_PartPredDFG.xml '+ MAPPER_KERNEL)
+  os.system('../../Release/CGRA_xml_compiler -d pedometer_INNERMOST_LN1_PartPred_DFG.xml -x 4 -y 4 -j hycube_original_mem.json -t HyCUBE_4REG')
+  os.system('cp *.bin '+ SIMULATOR_KERNEL)
+
+##############################################################################################################################################
+  print('\nRunning hycube_simulator\n')
+  os.chdir(SIMULATOR_KERNEL)
+
+  os.system('../../src/build/hycube_simulator *.bin loop_pedometer_INNERMOST_LN1_0.txt pedometer_INNERMOST_LN1_mem_alloc.txt')
+
+def my_mkdir(dir):
+    try:
+        os.mkdir(dir)
+    except:
+        pass
+
+if __name__ == '__main__':
+  main()
