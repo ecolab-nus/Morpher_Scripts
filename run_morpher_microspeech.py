d84f06571b3758880c6ea80c284dddb7e26738ec
DhananjayaWijerathne <dmdwijerathne@gmail.com>
2021-08-17 15:50:17 +0800

    Fix mem size in microspeech

--- run_morpher_microspeech.py
+++ run_morpher_microspeech.py
@@ -71,7 +71,7 @@ def main():
   print('\nRunning hycube_simulator\n')
   os.chdir(SIMULATOR_KERNEL)
 
-  os.system('../../src/build/hycube_simulator *.bin loop_microspeech_conv_layer_hycube_INNERMOST_LN13_0.txt microspeech_conv_layer_hycube_INNERMOST_LN13_mem_alloc.txt 16384')
+  os.system('../../src/build/hycube_simulator *.bin loop_microspeech_conv_layer_hycube_INNERMOST_LN13_0.txt microspeech_conv_layer_hycube_INNERMOST_LN13_mem_alloc.txt 32768')
 
 def my_mkdir(dir):
     try:
