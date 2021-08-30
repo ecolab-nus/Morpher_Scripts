aa0920b81dc8252eba7f7feb0d60962c72242c6e
thilinikb <thilinikbandara@gmail.com>
2021-08-30 17:34:17 +0800

    Adding 8x8 CGRA script

---
+++ pace8x8_16bit_fullchip/run_morpher_array_add.py
@@ -0,0 +1,83 @@
+  #!/usr/bin/env python
+import sys
+import os
+import os.path
+import shutil
+############################################
+# Directory Structure:
+# Morpher Home:
+#     -Morpher_DFG_Generator
+#     -Morpher_CGRA_Mapper
+#     -hycube_simulator
+#     -Morpher_Scripts
+
+# Build all three tools before running this script
+
+def main():
+
+  if not 'MORPHER_HOME' in os.environ:
+    raise Exception('Set MORPHER_HOME directory as an environment variable (Ex: export MORPHER_HOME=/home/dmd/Workplace/Morphor/github_ecolab_repos)')
+
+  MORPHER_HOME = os.getenv('MORPHER_HOME')
+  DFG_GEN_HOME = MORPHER_HOME + '/Morpher_DFG_Generator'
+  MAPPER_HOME = MORPHER_HOME + '/Morpher_CGRA_Mapper'
+  SIMULATOR_HOME = MORPHER_HOME + '/hycube_simulator'
+
+  DFG_GEN_KERNEL = DFG_GEN_HOME + '/applications/array_add/'
+  MAPPER_KERNEL = MAPPER_HOME + '/applications/hycube8x8_full/array_add/'
+  SIMULATOR_KERNEL =SIMULATOR_HOME + '/applications/hycube8x8_full/array_add/'
+
+  my_mkdir(DFG_GEN_KERNEL)
+  my_mkdir(MAPPER_KERNEL)
+  my_mkdir(SIMULATOR_KERNEL)
+
+
+
+##############################################################################################################################################
+  print('\nRunning Morpher_DFG_Generator\n')
+  os.chdir(DFG_GEN_KERNEL)
+
+  print('\nGenerating DFG\n')
+  os.system('./run_pass.sh array_add 8 8192')
+  os.system('dot -Tpdf array_add_INNERMOST_LN1_PartPredDFG.dot -o array_add_INNERMOST_LN1_PartPredDFG.pdf')
+
+  MEM_TRACE = DFG_GEN_KERNEL + '/memtraces'
+
+  my_mkdir(MEM_TRACE)
+
+  print('\nGenerating Data Memory Content\n')
+  os.system('./final')
+  os.system('cp memtraces/loop_array_add_INNERMOST_LN1_0.txt '+SIMULATOR_KERNEL)
+  os.system('cp array_add_INNERMOST_LN1_mem_alloc.txt '+SIMULATOR_KERNEL)
+  os.system('cp array_add_INNERMOST_LN1_mem_alloc.txt '+MAPPER_KERNEL)
+  os.system('cp array_add_INNERMOST_LN1_PartPred_DFG.xml '+ MAPPER_KERNEL)
+
+##############################################################################################################################################
+  print('\nRunning Morpher_CGRA_Mapper\n')
+  os.chdir(MAPPER_KERNEL)
+
+  os.system('rm *.bin')  
+  os.system('python ../../../update_mem_alloc.py ../../../json_arch/hycube_original_updatemem8x8.json array_add_INNERMOST_LN1_mem_alloc.txt 8192 8 hycube_original_mem.json')
+  print('\nupdate memory allocation done!\n')
+  os.system('../../../build/src/cgra_xml_mapper -d array_add_INNERMOST_LN1_PartPred_DFG.xml -x 8 -y 8 -j hycube_original_mem.json -t HyCUBE_4REG -i 4')
+
+  os.chdir(SIMULATOR_KERNEL)
+  os.system('rm *.bin')  
+
+  os.chdir(MAPPER_KERNEL)
+  os.system('cp *.bin '+ SIMULATOR_KERNEL)
+
+##############################################################################################################################################
+  print('\nRunning hycube_simulator\n')
+  os.chdir(SIMULATOR_KERNEL)
+
+  os.system('../../../src/build/hycube_simulator -c *.bin -d loop_array_add_INNERMOST_LN1_0.txt -a array_add_INNERMOST_LN1_mem_alloc.txt -m 65536 -x 8 -y 8 -t 2 > temp')
+
+def my_mkdir(dir):
+    try:
+        os.makedirs(dir) 
+    except:
+        pass
+
+if __name__ == '__main__':
+  main()
