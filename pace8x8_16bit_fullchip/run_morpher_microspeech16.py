4efa04b191f6b0e6be2265ed769695af9b509026
thilinikb <thilinikbandara@gmail.com>
2021-10-28 12:42:39 +0800

    Adding scripts for microspeech 16-bit

--- pace8x8_16bit_fullchip/run_morpher_microspeech16.py
+++ pace8x8_16bit_fullchip/run_morpher_microspeech16.py
@@ -3,6 +3,10 @@ import sys
 import os
 import os.path
 import shutil
+import re
+import numpy as np
+from tqdm import tqdm
+
 ############################################
 # Directory Structure:
 # Morpher Home:
@@ -24,8 +28,8 @@ def main():
   SIMULATOR_HOME = MORPHER_HOME + '/hycube_simulator'
 
   DFG_GEN_KERNEL = DFG_GEN_HOME + '/applications/hycube_v3_design_app_test/Microspeech16/'
-  MAPPER_KERNEL = MAPPER_HOME + '/applications/hycube/Microspeech16/'
-  SIMULATOR_KERNEL =SIMULATOR_HOME + '/applications/Microspeech16/'
+  MAPPER_KERNEL = MAPPER_HOME + '/applications/hycube8x8_full//Microspeech16/'
+  SIMULATOR_KERNEL =SIMULATOR_HOME + '/applications/hycube8x8_full/Microspeech16/'
 
   my_mkdir(DFG_GEN_KERNEL)
   my_mkdir(MAPPER_KERNEL)
@@ -33,12 +37,12 @@ def main():
 
 
 
-##############################################################################################################################################
+#############################################################################################################################################
   print('\nRunning Morpher_DFG_Generator\n')
   os.chdir(DFG_GEN_KERNEL)
 
   print('\nGenerating DFG\n')
-  os.system('./run_pass.sh microspeech_conv_layer_hycube 2 8192')
+  os.system('./run_pass.sh microspeech_conv_layer_hycube 8 8192')
   os.system('dot -Tpdf microspeech_conv_layer_hycube_INNERMOST_LN13_PartPredDFG.dot -o microspeech_conv_layer_hycube_INNERMOST_LN13_PartPredDFG.pdf')
 
   MEM_TRACE = DFG_GEN_KERNEL + '/memtraces'
@@ -47,7 +51,11 @@ def main():
 
   print('\nGenerating Data Memory Content\n')
   os.system('./final')
-  os.system('cp memtraces/loop_microspeech_conv_layer_hycube_INNERMOST_LN13_0.txt '+SIMULATOR_KERNEL)
+  list = os.listdir(MEM_TRACE)
+  num_memory_traces = 1#len(list)
+
+ # os.system('cp memtraces/loop_microspeech_conv_layer_hycube_INNERMOST_LN13_0.txt '+SIMULATOR_KERNEL)
+  os.system('cp -r memtraces/ '+SIMULATOR_KERNEL)
   os.system('cp microspeech_conv_layer_hycube_INNERMOST_LN13_mem_alloc.txt '+SIMULATOR_KERNEL)
   os.system('cp microspeech_conv_layer_hycube_INNERMOST_LN13_mem_alloc.txt '+MAPPER_KERNEL)
   os.system('cp microspeech_conv_layer_hycube_INNERMOST_LN13_PartPred_DFG.xml '+ MAPPER_KERNEL)
@@ -57,9 +65,9 @@ def main():
   os.chdir(MAPPER_KERNEL)
 
   os.system('rm *.bin') 
-  os.system('python ../../../update_mem_alloc.py ../../../json_arch/hycube_original_updatemem.json microspeech_conv_layer_hycube_INNERMOST_LN13_mem_alloc.txt 8192 2 hycube_original_mem.json')
+  os.system('python ../../../update_mem_alloc.py ../../../json_arch/hycube_original_updatemem8x8.json microspeech_conv_layer_hycube_INNERMOST_LN13_mem_alloc.txt 8192 8 hycube_original_mem.json')
   print('\nupdate memory allocation done!\n')
-  os.system('../../../build/src/cgra_xml_mapper -d microspeech_conv_layer_hycube_INNERMOST_LN13_PartPred_DFG.xml -x 4 -y 4 -j hycube_original_mem.json -i 12 -t HyCUBE_4REG')
+  os.system('../../../build/src/cgra_xml_mapper -d microspeech_conv_layer_hycube_INNERMOST_LN13_PartPred_DFG.xml -x 8 -y 8 -j hycube_original_mem.json -i 12 -t HyCUBE_4REG')
   
   os.chdir(SIMULATOR_KERNEL)
   os.system('rm *.bin')  
@@ -70,8 +78,29 @@ def main():
 ##############################################################################################################################################
   print('\nRunning hycube_simulator\n')
   os.chdir(SIMULATOR_KERNEL)
-
-  os.system('../../src/build/hycube_simulator -c *.bin -d loop_microspeech_conv_layer_hycube_INNERMOST_LN13_0.txt -a microspeech_conv_layer_hycube_INNERMOST_LN13_mem_alloc.txt -m 16384')
+  #list = os.listdir('memtraces')
+  #num_memory_traces = 1#len(list)
+  for invocations in range(0,num_memory_traces) :
+    data_file = SIMULATOR_KERNEL + '/memtraces/loop_microspeech_conv_layer_hycube_INNERMOST_LN13_' + str(invocations) + '.txt'
+    os.system('python3 ../../scripts/skipdata.py --cubedata '+data_file)
+    data_file_new = 'data_modi_' + str(invocations) + '.txt'
+    os.system('cp data_modi.txt '+data_file_new)
+  
+  invocation = num_memory_traces
+  
+  for i in range(0,invocation) :
+    os.system('../../../src/build/hycube_simulator -c *.bin -d data_modi_' + str(i) +'.txt -a microspeech_conv_layer_hycube_INNERMOST_LN13_mem_alloc.txt -m 65536 -x 8 -y 8 -t 2 | tail -n 2 |head -n 1 > output.log')
+    with open("output.log", 'r') as f:
+      line = f.readline()
+      success = False
+      while line:
+        if re.search("Mismatches::0", line):
+          success = True
+          line = f.readline()
+      if not success:
+        print("ERROR: FAIL AT MEMTRACE "+ i)
+  print("Done!")
+    
 
 def my_mkdir(dir):
     try:
